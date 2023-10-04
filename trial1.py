a65e99063132c27aae091e1ed19d2cf861e7b135
manish <manumi296@gmail.com>
2023-10-04 15:16:46 +0530

    working Good

--- trial1.py
+++ trial1.py
@@ -22,3 +22,14 @@ first_input_prompt = PromptTemplate(
     input_variables=['name'],
     template="Tell me about {name}"
 )
+
+llm=OpenAI(temperature=0.8)
+chain = LLMChain(llm=llm, prompt=first_input_prompt,verbose=True,output_key='person')
+
+
+parent_chain = SequentialChain(
+    chains=[chain],input_variables=['name'],output_variables=['person'],verbose=True
+)
+
+if input_text:
+    st.write(parent_chain({'name':input_text}))
